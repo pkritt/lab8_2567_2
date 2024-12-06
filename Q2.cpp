128d2b681f742b46139f596bd01d3a9c9b48f13d
pkritt <krittapat37819@gmail.com>
2024-12-06 15:19:10 +0700

    finish

---
+++ Q2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+
+char findGrade(double x){
+    if(x>=91){
+        return 'A';}
+        else if(x<=90&&x>=76){
+        return 'B';}
+        else if(x<=75&&x>=61){
+        return 'C';}
+        else if(x<=60&&x>=46){
+        return 'D';}
+        else if(x<=45){
+        return 'F';}
+}
+int main(){
+    
+cout << findGrade(91);
+
+return 0;
+}
---
+++ lab8_2.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+char findGrade(double x){
+    if(x>90){
+        return 'A';}
+        else if(x<=90&&x>75){
+        return 'B';}
+        else if(x<=75&&x>60){
+        return 'C';}
+        else if(x<=60&&x>45){
+        return 'D';}
+        else{
+        return 'F';}
+}
+
+
+int main(){
+	//Input the number of students
+	int N,i = 0;
+	cout << "Enter the number of students: ";
+	cin >> N;
+	string name[N];
+	float score[N];	
+	
+	//Store names and scores of students into an array 
+	while(i < N){
+		cout << "Name of student " << i+1 << ": ";
+		cin.ignore();
+		getline(cin,name[i]);
+		cout << "Score of student " << i+1 << ": ";
+		//[Missing Code 3] Get score of the i-th students.
+        cin >> score[i];
+		i++;
+	}
+	
+	//Print names scores and grades
+	i = 0;
+	cout << "---------------------------------------------\n";
+	cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
+	cout << "---------------------------------------------\n";
+	while(i < N){
+		cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
+		i++;
+	} 
+	cout << "---------------------------------------------\n";
+
+	return 0;
+}
